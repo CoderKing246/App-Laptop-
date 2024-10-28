1db6d84494f8cb9a609dcc530c0e4e1dbd198c9b
CoderKing246 <mehrahitesh2006@gmail.com>
2024-10-28 16:44:52 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-import streamlit as st  
-import pickle 
-import numpy as np 
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd  # Make sure to import pandas
 
-# Load the trained model
+# Load the model and DataFrame
 with open('pipe.pkl', 'rb') as f:
     pipe = pickle.load(f)
 
-with open('df.pkl', 'rb') as f:
-    df = pickle.load(f)
+with open('df.pkl', 'rb') as f1:
+    df = pickle.load(f1)
 
-st.title('Laptop Price Predictor')
+st.title("Laptop Price Predictor")
 
-# Brand 
+# Brand
 company = st.selectbox('Brand', df['Company'].unique())
 
-# Type
-type = st.selectbox('Type', df['TypeName'].unique())
+# Type of laptop
+type_ = st.selectbox('Type', df['TypeName'].unique())
 
 # RAM
 ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
 
 # Weight
-weight = st.number_input('Weight of the laptop')
+weight = st.number_input('Weight of the Laptop', min_value=0.0)
 
 # Touchscreen
 touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
@@ -30,51 +31,44 @@ touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
 ips = st.selectbox('IPS', ['No', 'Yes'])
 
 # Screen size
-screen_size = st.number_input('Screen Size')
+screen_size = st.slider('Screen size in inches', 10.0, 18.0, 13.0)
 
 # Resolution
 resolution = st.selectbox('Screen Resolution', [
-    '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800',
-    '2880x1800', '2560x1600', '2560x1440', '2304x1440'
+    '1920x1080', '1366x768', '1600x900', '3840x2160',
+    '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440'
 ])
 
 # CPU
 cpu = st.selectbox('CPU', df['Cpu Brand'].unique())
 
-# HDD
+# HDD and SSD
 hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
-
-# SSD
 ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
 
 # GPU
-gpu = st.selectbox('GPU', df['Gpu Brand'].unique())
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
 
 # OS
 os = st.selectbox('OS', df['os'].unique())
 
+# Prediction button
 if st.button('Predict Price'):
-    # Prepare query
-    ppi = None
-    if touchscreen == 'Yes':
-        touchscreen = 1
-    else:
-        touchscreen = 0
+    # Query
+    touchscreen = 1 if touchscreen == 'Yes' else 0
+    ips = 1 if ips == 'Yes' else 0
 
-    if ips == 'Yes':
-        ips = 1
-    else:
-        ips = 0
-
-    # Calculate Pixels Per Inch (PPI)
     X_res = int(resolution.split('x')[0])
     Y_res = int(resolution.split('x')[1])
-    ppi = ((X_res**2) + (Y_res**2))**0.5 / screen_size
+    ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5 / screen_size
 
-    # Arrange query in the correct shape for prediction
-    query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
-    query = query.reshape(1, -1)  # Reshape to match model input shape
+    # Prepare the query for prediction as a DataFrame
+    query = pd.DataFrame([[company, type_, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os]], 
+                        columns=['Company', 'TypeName', 'Ram(GB)', 'Weight(kg)', 'Touch Screen', 'Ips', 
+                                    'ppi', 'Cpu Brand', 'HDD', 'SSD', 'Gpu brand', 'os'])
 
-    # Predict and display the result
-    predicted_price = np.exp(pipe.predict(query)[0])  # Use exponential if the model predicts log prices
-    st.title("The predicted price of this configuration is $" + str(int(predicted_price)))
+    try:
+        predicted_price = int(np.exp(pipe.predict(query)[0]))
+        st.title(f"The predicted price of this configuration is ${predicted_price}")
+    except Exception as e:
+        st.error(f"Error in prediction: {str(e)}")
